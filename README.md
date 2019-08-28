ea9bb402fd01c0b61054fa4b40382157c0d1f3c1
Shen Lin <shen.lin@outlook.com>
2019-08-28 10:30:45 -0700

    add local rocketmq start info

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+## Build
+
 ./gradlew.bat clean build
 
 ## Watch src file changes and continously build. Run in two separate terminals.
 ./gradlew.bat -t build 
 ./gradlew.bat bootRun
+
+## Start RocketMQ locally
+
+docker run -d -p 9876:9876 -v ~/Development/local/rocketmq-volume/data/namesrv/logs:/home/rocketmq/logs -v ~/Development/local/rocketmq-volume/data/namesrv/store:/home/rocketmq/store --name rmqnamesrv -e "MAX_POSSIBLE_HEAP=100000000" rocketmqinc/rocketmq:4.4.0 sh mqnamesrv -n 10.0.75.1:9876
+
+docker run -d -p 10911:10911 -p 10909:10909 -v ~/Development/local/rocketmq-volume/data/broker/logs:/home/rocketmq/logs -v ~/Development/local/rocketmq-volume/data/broker/store:/home/rocketmq/store -v ~/Development/local/rocketmq-volume/conf/broker.conf:/opt/rocketmq-4.4.0/conf/broker.conf --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" -e "MAX_POSSIBLE_HEAP=200000000" rocketmqinc/rocketmq:4.4.0 sh mqbroker autoCreateTopicEnable=true -n 10.0.75.1:9876 -c conf/broker.conf
+
+
+docker exec -i -t CONTAINER_ID /bin/bash
--- src/main/java/shenlin84/katacoda/scenarios/distributedtransactionjavascenario/controller/DemoController.java
+++ src/main/java/shenlin84/katacoda/scenarios/distributedtransactionjavascenario/controller/DemoController.java
@@ -8,18 +8,45 @@ import org.springframework.web.bind.annotation.*;
 import shenlin84.katacoda.scenarios.distributedtransactionjavascenario.model.UserAccount;
 
 import java.util.*;
+import org.apache.rocketmq.client.producer.*;
+import org.apache.rocketmq.common.message.*;
+import org.apache.rocketmq.remoting.common.*;
 
 @RestController
 public class DemoController {
 
-
     public DemoController() {
+
+    }
+
+    /**
+     * Send test message to local RMQ
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/testRMQ", method = RequestMethod.POST)
+    public ResponseEntity<String> testRMQ() throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+        producer.setNamesrvAddr("localhost:9876");
+
+        try {
+            producer.start();
+            Message msg = new Message("TBW102", "TagTest", ("Hello RMQ").getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.println("SendResult: " + sendResult.getSendStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.shutdown();
+        }
+        System.out.println("Send response no matter what!!!");
+        return new ResponseEntity<String>("Test Succeeded", HttpStatus.OK);
     }
 
     @RequestMapping(value = "/createDB", method = RequestMethod.POST)
     public ResponseEntity<String> createDB() {
         try {
-            return new ResponseEntity<String>("DB created", HttpStatus.ACCEPTED);
+            return new ResponseEntity<String>("DB created", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<String>("DB creation failed", HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -29,8 +56,8 @@ public class DemoController {
     public ResponseEntity<String> createUser(@RequestBody UserAccount userAccount) {
         try {
 
-            System.out.println(userAccount.getUser() + ' ' +  userAccount.getBalance() + ' ' +  userAccount.getBank());
-            return new ResponseEntity<String>("Entity created", HttpStatus.ACCEPTED);
+            System.out.println(userAccount.getUser() + ' ' + userAccount.getBalance() + ' ' + userAccount.getBank());
+            return new ResponseEntity<String>("Entity created", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<String>("Entity creation failed", HttpStatus.INTERNAL_SERVER_ERROR);
         }
