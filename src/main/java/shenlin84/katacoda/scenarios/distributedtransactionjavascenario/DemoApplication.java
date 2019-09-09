427ceff3c65c42b3d1736905b82d8ec91b589626
Shen Lin <shen.lin@outlook.com>
2019-09-08 23:00:26 -0700

    fix db table auto create

--- src/main/java/shenlin84/katacoda/scenarios/distributedtransactionjavascenario/DemoApplication.java
+++ src/main/java/shenlin84/katacoda/scenarios/distributedtransactionjavascenario/DemoApplication.java
@@ -1,12 +1,10 @@
 package shenlin84.katacoda.scenarios.distributedtransactionjavascenario;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ComponentScan
 public class DemoApplication {
 
